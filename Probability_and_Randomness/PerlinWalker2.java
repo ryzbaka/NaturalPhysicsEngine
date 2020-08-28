0c5c44da445897fa6e9e4910c2fc41f795133e99
Hamza Ali Rizvi <hamzaali151214@hotmail.com>
2020-08-29 03:07:42 +0530

    perlin noise walker with fading tails and white strokes

---
+++ Probability_and_Randomness/PerlinWalker2.java
@@ -0,0 +1,48 @@
+//perlin noise random walker
+import java.util.ArrayList;
+class PerlinWalker{
+  float t = random(0,100);
+  float xPos;
+  float yPos;
+  float r;
+  float g;
+  float b;
+  float speed;
+  public PerlinWalker(float r,float g,float b,float speed){
+    this.r=r;
+    this.g=g;
+    this.b=b;
+    this.speed = speed;
+  }
+  public void show(){
+    this.t+=this.speed;
+    this.xPos = map(noise(this.t),0,1,0,width);
+    this.yPos = map(noise(this.t,this.t),0,1,0,height);
+    fill(this.r,this.g,this.b);
+    stroke(255);
+    ellipse(this.xPos,this.yPos,40,40);
+  }
+}
+ArrayList<PerlinWalker> walkers = new ArrayList<PerlinWalker>();
+int numWalkers = 10;
+
+void setup(){
+  size(1920,1080);
+  background(0);
+  for(int i=0;i<numWalkers;i++){
+    PerlinWalker  walker = new 
+PerlinWalker(random(0,255),random(0,255),random(0,255),0.02);
+    walkers.add(walker);
+  }
+  frameRate(60);
+}
+void draw(){
+  //background(0);
+  fill(0,0,0,50);
+  rect(0,0,width,height);
+  for(int i=0;i<numWalkers;i++){
+    PerlinWalker walker = walkers.get(i);
+    walker.show();
+  }
+}
+
